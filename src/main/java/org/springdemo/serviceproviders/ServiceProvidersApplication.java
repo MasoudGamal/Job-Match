37f848958b43ea354b7c5c6fbbac4181b09971b2
mas3od gamal <masuodgamal00@gmail.com>
2024-12-28 23:02:41 +0200

    initial commit

---
+++ src/main/java/org/springdemo/serviceproviders/ServiceProvidersApplication.java
@@ -0,0 +1,13 @@
+package org.springdemo.serviceproviders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProvidersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProvidersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/springdemo/serviceproviders/ServiceProvidersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springdemo.serviceproviders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProvidersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
